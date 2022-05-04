ca8c9515b3968deb53b38a19b55492f45cee8313
Melanie233 <101596711+Melanie233@users.noreply.github.com>
2022-05-04 00:03:24 -0500

    Add files via upload

---
+++ Atributos.java
@@ -0,0 +1,46 @@
+package archivoslist;
+
+/**
+ *
+ * @author Hernan Ponce Tapia
+ */
+public class Atributos {
+    
+    String Nombre;
+    String Telefono;
+    String Correo;
+    String Profesion;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+    public String getProfesion() {
+        return Profesion;
+    }
+
+    public void setProfesion(String Profesion) {
+        this.Profesion = Profesion;
+    }
+    
+    
+}
---
+++ info.java
@@ -0,0 +1,41 @@
+  public class info {
+    
+    String Nombre;
+    String Telefono;
+    String Correo;
+    String Profesion;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+    public String getProfesion() {
+        return Profesion;
+    }
+
+    public void setProfesion(String Profesion) {
+        this.Profesion = Profesion;
+    }
+    
+    
+}
+
